c265003f3034d916a6c6dc639f79215629afbf4f
dmtuan1 <dmtuan1@tma.com.vn>
2023-09-11 16:46:59 +0700

    init subnet

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ terraform {
     bucket                  = "dc11-dot-van-le-networking"
     key                     = "my-terraform"
     region                  = "us-east-1"
-    shared_credentials_files = "~/.aws/credentials"
+    shared_credentials_file = "~/.aws/credentials"
   }
 }
 
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_files = "~/.aws/credentials"
+  shared_credentials_file = "~/.aws/credentials"
 }
 
 resource "aws_vpc" "main" {
