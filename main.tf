99736b9c82d663aa8980471ba06ff369593fb6f7
dmtuan1 <dmtuan1@tma.com.vn>
2023-09-12 11:46:22 +0700

    init terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     encrypt                 = true
-    profile                 = default
-    bucket                  = "dc11-dot-van-le-networking"
     key                     = "networking/terraform.tfstate"
+    bucket                  = "dc11-dot-van-le-networking"
+    profile                 = "default"
     region                  = "us-east-1"
     dynamodb_table = "terraform-state-dynamodb-table-lock"
   }
