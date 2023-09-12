0f3d1cdd2405750b747fa775f6cbf8f3b19e6197
dmtuan1 <dmtuan1@tma.com.vn>
2023-09-12 14:38:36 +0700

    init terraform

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = "~/.aws/credentials"
+  profile                  = "default"
 }
 
 resource "aws_s3_bucket" "terraform_state_s3_bucket" {
