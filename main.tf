5fce90aa8dbd912d033fbbe2a78ed3a28accd840
dmtuan1 <dmtuan1@tma.com.vn>
2023-09-12 08:36:02 +0700

    init subnet

--- main.tf
+++ main.tf
@@ -63,3 +63,11 @@ resource "aws_route_table_association" "public_subnet_asso" {
  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
  route_table_id = aws_route_table.second_rt.id
 }
+
+resource "aws_egress_only_internet_gateway" "eogw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Networking VPC EOGW"
+  }
+}
