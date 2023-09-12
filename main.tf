91eba8359f7ff3e6ce9e8c68c543e2319d390169
dmtuan1 <dmtuan1@tma.com.vn>
2023-09-12 09:17:06 +0700

    init subnet

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_files = "~/.aws/credentials"
+  shared_credentials_file = "~/.aws/credentials"
 }
 
 resource "aws_vpc" "main" {
